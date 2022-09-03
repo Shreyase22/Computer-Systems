44b0faa8cc06ce879c69e76c6398a20f2c43537c
Shreyase22 <112756620+Shreyase22@users.noreply.github.com>
2022-09-03 16:32:01 +0530

    Add files via upload

---
+++ hello_arm.s.s
@@ -0,0 +1,44 @@
+	.cpu arm7tdmi
+	.arch armv4t
+	.fpu softvfp
+	.eabi_attribute 20, 1
+	.eabi_attribute 21, 1
+	.eabi_attribute 23, 3
+	.eabi_attribute 24, 1
+	.eabi_attribute 25, 1
+	.eabi_attribute 26, 1
+	.eabi_attribute 30, 6
+	.eabi_attribute 34, 0
+	.eabi_attribute 18, 4
+	.file	"hell.c"
+	.text
+	.section	.rodata
+	.align	2
+.LC0:
+	.ascii	"\012 Hello\000"
+	.text
+	.align	2
+	.global	main
+	.syntax unified
+	.arm
+	.type	main, %function
+main:
+	@ Function supports interworking.
+	@ args = 0, pretend = 0, frame = 0
+	@ frame_needed = 1, uses_anonymous_args = 0
+	push	{fp, lr}
+	add	fp, sp, #4
+	ldr	r0, .L3
+	bl	printf
+	mov	r3, #0
+	mov	r0, r3
+	sub	sp, fp, #4
+	@ sp needed
+	pop	{fp, lr}
+	bx	lr
+.L4:
+	.align	2
+.L3:
+	.word	.LC0
+	.size	main, .-main
+	.ident	"GCC: (GNU Arm Embedded Toolchain 10.3-2021.10) 10.3.1 20210824 (release)"
